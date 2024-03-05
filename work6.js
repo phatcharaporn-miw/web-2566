1dabbdb1e0bb53feabccd01d6bf101598cf71a38
phatcharaporn-miw24 <phatcharaporn.ni@kkumail.com>
2024-03-05 15:46:47 +0700

    work6

--- work6.js
+++ work6.js
@@ -1,12 +1,12 @@
-import Card from 'react-bootstrap/Card';
+
 const firebaseConfig = {
-    apiKey: "AIzaSyAEnPP1zMx30n-BS0XTciEzn3Il6xG9IYg",
-    authDomain: "web2566-34401.firebaseapp.com",
-    projectId: "web2566-34401",
-    storageBucket: "web2566-34401.appspot.com",
-    messagingSenderId: "126920306114",
-    appId: "1:126920306114:web:ae824988d4dd744347ba5b",
-    measurementId: "G-C2H8HZW093"
-  };
+  apiKey: "AIzaSyAEnPP1zMx30n-BS0XTciEzn3Il6xG9IYg",
+  authDomain: "web2566-34401.firebaseapp.com",
+  projectId: "web2566-34401",
+  storageBucket: "web2566-34401.appspot.com",
+  messagingSenderId: "126920306114",
+  appId: "1:126920306114:web:ae824988d4dd744347ba5b",
+  measurementId: "G-C2H8HZW093"
+};
 firebase.initializeApp(firebaseConfig);      
 const db = firebase.firestore();
